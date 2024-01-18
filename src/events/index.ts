faf17faaffc3e3cb50753bfcc9b5ca6a35beead0
Kevin Dang <kevinthedang_1@outlook.com>
2024-01-18 14:00:27 -0800

    bot can create a message

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,7 +1,9 @@
 import { Event } from '../utils/index.js'
+import notifygit from './notifygit.js'
 import ready from './ready.js'
 
 // Centralized export for all events
 export default [
-    ready
+    ready,
+    notifygit
 ] as Event[] // static is better ts practice, dynamic exporting is possible
---
+++ src/events/notifygit.ts
@@ -0,0 +1,18 @@
+import { event, Events } from "../utils/events.js"
+
+export default event(Events.MessageCreate, ({ log }, message) => {
+    log(`Command Received: \"${message.content}\' from ${message.author.tag}`)
+
+    // check channelId
+    if (message.channelId != '1197245823759687761') {
+        log('Unauthorized Channel input, aborting...')
+        return
+    }
+
+    // check if not the bot, do not respond to it
+    if (message.author.tag === message.client.user.tag) return
+
+    // reply to user with static message for now
+    message.reply(`Command Received from ${message.author.tag}`)
+})
+
